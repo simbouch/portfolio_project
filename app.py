0338d2e181fa4e73603b577921a0d609811fbf7d
simbouch <simplonbouchaib@gmail.com>
2024-11-13 11:48:03 +0100

    removing the procfile andtrying the app.py

--- app.py
+++ app.py
@@ -2,11 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# Define routes
-@app.route('/')
-def home():
-    return "Hello, World!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)  # Optional for local testing
+    app.run()  
 
