3e7a6d5b37145f60302b82a995b6c5248ebc61f5
simbouch <simplonbouchaib@gmail.com>
2024-11-13 11:30:57 +0100

    app.py solve issue

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 
-app = Flask(__name__)  # Flask app instance named 'app'
+app = Flask(__name__)
 
-# Define your routes here
+# Define routes
 @app.route('/')
 def home():
     return "Hello, World!"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=8000)  # Optional for local testing
+
