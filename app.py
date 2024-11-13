229b6040d4fe716d5c7446014caf0831f6114a89
simbouch <simplonbouchaib@gmail.com>
2024-11-13 11:29:47 +0100

    app.py issue to solve

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-# portfolio_project/run.py
-
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__)  # Flask app instance named 'app'
 
 # Define your routes here
+@app.route('/')
+def home():
+    return "Hello, World!"
 
 if __name__ == "__main__":
     app.run()
