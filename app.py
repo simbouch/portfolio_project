454295d6ce4cd7a7549efb98dfd3f65317c6014f
simbouch <simplonbouchaib@gmail.com>
2024-11-14 12:55:18 +0100

    commit test local

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-# app.py
-from flask import Flask
+# portfolio_project/run.py
 
-app = Flask(__name__)
+from app import create_app
+import webbrowser
+from threading import Timer
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+app = create_app()
 
-if __name__ == "__main__":
+def open_browser():
+    """Open the default web browser on the localhost."""
+    webbrowser.open_new("http://127.0.0.1:5000/")
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    # Start a timer to open the browser after Flask starts
+    Timer(1, open_browser).start()
     app.run()
