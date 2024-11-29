6d1e757c4f969394bff5801a19d9ea65fc80743e
simbouch <simplonbouchaib@gmail.com>
2024-11-29 11:03:43 +0100

    changing the app.py  with specify the  port 8000

--- app.py
+++ app.py
@@ -1,17 +1,6 @@
-# portfolio_project/run.py
-
 from app import create_app
-import webbrowser
-from threading import Timer
 
-app = create_app()
+app = create_app()  # Ensure the `create_app` function exists and initializes the Flask app
 
-def open_browser():
-    """Open the default web browser on the localhost."""
-    webbrowser.open_new("http://127.0.0.1:5000/")
-
-if __name__ == '__main__':
-    app.run(debug=True)
-    # Start a timer to open the browser after Flask starts
-    Timer(1, open_browser).start()
-    app.run()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)  # Bind to all network interfaces on port 8000
